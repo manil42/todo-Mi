f6952eace3a506040b15716b6b82ccb4a8f649b0
milanbhandari047 <milanbhandari047@gmail.com>
2024-02-29 20:10:12 +0545

    handleDelete function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Card from "./components/Card/Card";
+import Button from "./components/Button/Button";
+import { SassNumber } from "sass";
 function App() {
   const [post, setPosts] = useState([]);
   useEffect(() => {
@@ -11,16 +13,27 @@ function App() {
 
   [];
 
+  const handleAdd = () => {
+    //@ts-ignore
+    setPosts([...post, { id: 3, title: "todo3", description: "hello" }]);
+  };
+
   const handleEdit = () => {
     console.log("Edit button is clicked");
   };
-  const handleDelete = () => {
-    console.log("Delete button is clicked");
+  const handleDelete = (id: number) => {
+    console.log("id", id);
+    const copyData = [...post];
+    const data = copyData.filter((data: any) => data.id !== id);
+    setPosts([...data]);
   };
 
   return (
     <>
       <div>
+        <div style={{ margin: "16px" }}>
+          <Button label="Add Todo" handleClick={handleAdd} />
+        </div>
         {post?.map((data: any) => (
           // <div style={{border:"solid 1px black"}}>
           // <h1>{data.title}</h1>
@@ -33,7 +46,9 @@ function App() {
               handleEdit={() => {
                 handleEdit;
               }}
-              handleDelete={handleDelete}
+              handleDelete={() => {
+                handleDelete(data.id);
+              }}
             />
           </div>
         ))}
